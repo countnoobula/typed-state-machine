60675c91ce44461300d474ffc4e1f5b4eb0bd38c
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2019-10-30 16:46:23 +0200

    Fix StateMachine@fireEvent functionality
    I forgot to use $class instead of the instance.
    This resolves it, and also hides some PHPStorm warnings about how we
    call it.

--- src/TypedStateMachines/StateMachine.php
+++ src/TypedStateMachines/StateMachine.php
@@ -220,15 +220,20 @@ abstract class StateMachine implements IStateMachine
     /**
      * Fires the provided event into the bus.
      *
+     * @param IEvent $event
      * @return void
      */
     public function fireEvent(IEvent $event)
     {
-        $class = get_class($event);
-        $listeners = $this->getListeners()[$event];
+        $class     = get_class($event);
+        $listeners = $this->getListeners();
 
-        foreach ($listeners as $listener) {
-            (new $listener())->handle($event);
+        if (array_key_exists($class, $listeners)) {
+            foreach ($listeners[$class] as $listener) {
+                /** @var IListener $listenerInstance */
+                $listenerInstance = new $listener();
+                $listenerInstance->handle($event);
+            }
         }
     }
 
